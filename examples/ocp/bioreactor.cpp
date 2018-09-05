770e98b8aab6e808a743cb48f0b69bd9a6aa2a8d
Bruno Brito <bruno.debrito@tudelft.nl>
2018-09-05 14:52:16 +0200

    adding obstacle collision avoidance

--- examples/ocp/bioreactor.cpp
+++ examples/ocp/bioreactor.cpp
@@ -41,8 +41,8 @@ int main( ){
 
     // INTRODUCE THE VARIABLES:
     // -------------------------
-    DifferentialState     x,y,psi,v, s;//, dummy;
-    Control               a,delta;//, sv;
+    DifferentialState     x,y,psi,v, s,dummy;
+    Control               a,delta,sv;
     DifferentialEquation  f    ;
 
 	OnlineData a_X1;
@@ -70,10 +70,30 @@ int main( ){
 
 	OnlineData s01;
 	OnlineData s02;
-	OnlineData vref;
+	OnlineData vref1;
+	OnlineData vref2;
+
 	OnlineData d;
 
-	Expression lambda = 1/(1 + exp((s - d)/0.05));
+	OnlineData ws;
+	OnlineData wP;
+
+	OnlineData r_disc;
+	OnlineData disc_pos;
+
+	OnlineData obst1_x;
+	OnlineData obst1_y;
+	OnlineData obst1_theta;
+	OnlineData obst1_major;
+	OnlineData obst1_minor;
+
+	OnlineData obst2_x;
+	OnlineData obst2_y;
+	OnlineData obst2_theta;
+	OnlineData obst2_major;
+	OnlineData obst2_minor;
+
+	Expression lambda = 1/(1 + exp((s - d)/0.1));
 
 	Expression x_path1 = (a_X1*(s-s01)*(s-s01)*(s-s01) + b_X1*(s-s01)*(s-s01) + c_X1*(s-s01) + d_X1) ;
 	Expression y_path1 = (a_Y1*(s-s01)*(s-s01)*(s-s01) + b_Y1*(s-s01)*(s-s01) + c_Y1*(s-s01) + d_Y1) ;
@@ -94,6 +114,8 @@ int main( ){
 	Expression dx_path_norm = dx_path/abs_grad;
 	Expression dy_path_norm =  dy_path/abs_grad;
 
+	Expression vref = lambda*vref1 + (1 - lambda)*vref2;
+
 	double lr=1.123; // distance from center of mass of the vehicle to the rear
 	double lf=1.577; // distance from center of mass of the vehicle to the front
 	double ratio =lr/(lf+lr);
@@ -107,35 +129,76 @@ int main( ){
     f << dot(psi) == v/lr*sin(beta);
     f << dot(v) == a;
     f << dot(s) == v;
+    f << dot(dummy) == sv;
 
     // DEFINE AN OPTIMAL CONTROL PROBLEM:
     // ----------------------------------
     OCP ocp( 0.0, 5.0, 25.0 );
 
     // Need to set the number of online variables!
-    ocp.setNOD(25);
+    ocp.setNOD(38);
 
 	Expression error_contour   = dy_path_norm * (x - x_path) - dx_path_norm * (y - y_path);
 
 	Expression error_lag       = -dx_path_norm * (x - x_path) - dy_path_norm * (y - y_path);
 
+	ocp.minimizeLagrangeTerm(Wx*error_contour*error_contour + Wy*error_lag*error_lag + ws*sv*sv + Ww*w*w +Wv*(v-vref)*(v-vref) + wP*((1/((x-obst1_x)*(x-obst1_x)+(y-obst1_y)*(y-obst1_y)+0.0001)) + (1/((x-obst2_x)*(x-obst2_x)+(y-obst2_y)*(y-obst2_y)+0.0001)))); // weight this with the physical cost!!!
+	ocp.setModel(f);
 
-	ocp.minimizeLagrangeTerm(Wx*error_contour*error_contour + Wy*error_lag*error_lag + Wdelta*delta*delta +Wa*(v-vref)*(v-vref));// weight this with the physical cost!!!
-    ocp.subjectTo( f );
-	//ocp.setModel(f);
-
-    ocp.subjectTo( -4 <= a <= 1.5 );
-    ocp.subjectTo( -0.52 <= delta <= 0.52 );
+    	ocp.subjectTo( -4 <= a <= 1.5 );
+    	ocp.subjectTo( -0.52 <= delta <= 0.52 );
 	ocp.subjectTo( 0 <= v <= 13.8 );
+
+    	// DEFINE COLLISION CONSTRAINTS:
+	// ---------------------------------------
+
+	Expression ab_1(2,2);
+	ab_1(0,0) = 1/((obst1_major + r_disc)*(obst1_major + r_disc));
+	ab_1(0,1) = 0;
+	ab_1(1,1) = 1/((obst1_minor + r_disc)*(obst1_minor + r_disc));
+	ab_1(1,0) = 0;
+
+	Expression ab_2(2,2);
+	ab_2(0,0) = 1/((obst2_major + r_disc)*(obst2_major + r_disc));
+	ab_2(0,1) = 0;
+	ab_2(1,1) = 1/((obst2_minor + r_disc)*(obst2_minor + r_disc));
+	ab_2(1,0) = 0;
+
+	Expression R_obst_1(2,2);
+	R_obst_1(0,0) = cos(obst1_theta);
+	R_obst_1(0,1) = -sin(obst1_theta);
+	R_obst_1(1,0) = sin(obst1_theta);
+	R_obst_1(1,1) = cos(obst1_theta);
+
+	Expression R_obst_2(2,2);
+	R_obst_2(0,0) = cos(obst2_theta);
+	R_obst_2(0,1) = -sin(obst2_theta);
+	R_obst_2(1,0) = sin(obst2_theta);
+	R_obst_2(1,1) = cos(obst2_theta);
+
+	Expression deltaPos_disc_1(2,1);
+	deltaPos_disc_1(0) =  x - obst1_x;
+	deltaPos_disc_1(1) =  y - obst1_y;
+
+	Expression deltaPos_disc_2(2,1);
+	deltaPos_disc_2(0) =  x - obst2_x;
+	deltaPos_disc_2(1) =  y - obst2_y;
+
+	Expression c_obst_1, c_obst_2;
+	c_obst_1 = deltaPos_disc_1.transpose() * R_obst_1.transpose() * ab_1 * R_obst_1 * deltaPos_disc_1;
+	c_obst_2 = deltaPos_disc_2.transpose() * R_obst_2.transpose() * ab_2 * R_obst_2 * deltaPos_disc_2;
+
+	ocp.subjectTo(c_obst_1 - sv >= 1);
+	ocp.subjectTo(c_obst_2 - sv >= 1);
+
 
 	// DEFINE AN MPC EXPORT MODULE AND GENERATE THE CODE:
 	// ----------------------------------------------------------
 	OCPexport mpc( ocp );
 
 	mpc.set( HESSIAN_APPROXIMATION,       EXACT_HESSIAN  		);
-    mpc.set( DISCRETIZATION_TYPE,         MULTIPLE_SHOOTING 	);
-	mpc.set( INTEGRATOR_TYPE,             INT_RK4			    );
-//    mpc.set( INTEGRATOR_TYPE,             INT_IRK_GL4			);
+    	mpc.set( DISCRETIZATION_TYPE,         MULTIPLE_SHOOTING 	);
+	mpc.set( INTEGRATOR_TYPE,             INT_RK4			);
 	mpc.set( NUM_INTEGRATOR_STEPS,        25            		);
 	mpc.set( QP_SOLVER,                   QP_QPOASES    		);
 	mpc.set( HOTSTART_QP,                 NO             		);
