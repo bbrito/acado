68e24c31c6a7a10f66fc3fd4fd8832f247cf12bf
Bruno Brito <bruno.debrito@tudelft.nl>
2018-09-09 17:43:42 +0200

    reference velocity cost replaced by negative velocity cost

--- examples/ocp/bioreactor.cpp
+++ examples/ocp/bioreactor.cpp
@@ -142,7 +142,7 @@ int main( ){
 
 	Expression error_lag       = -dx_path_norm * (x - x_path) - dy_path_norm * (y - y_path);
 
-	ocp.minimizeLagrangeTerm(Wx*error_contour*error_contour +a*a+ Wy*error_lag*error_lag + ws*sv*sv + Wdelta*delta*delta +Wa*(v-vref)*(v-vref) + wP*((1/((x-obst1_x)*(x-obst1_x)+(y-obst1_y)*(y-obst1_y)+0.0001)) + (1/((x-obst2_x)*(x-obst2_x)+(y-obst2_y)*(y-obst2_y)+0.0001)))); // weight this with the physical cost!!!
+	ocp.minimizeLagrangeTerm(Wx*error_contour*error_contour +a*a+ Wy*error_lag*error_lag + ws*sv*sv + Wdelta*delta*delta -Wa*v + wP*((1/((x-obst1_x)*(x-obst1_x)+(y-obst1_y)*(y-obst1_y)+0.0001)) + (1/((x-obst2_x)*(x-obst2_x)+(y-obst2_y)*(y-obst2_y)+0.0001)))); // weight this with the physical cost!!!
 	ocp.setModel(f);
 
     	ocp.subjectTo( -4 <= a <= 1.5 );
