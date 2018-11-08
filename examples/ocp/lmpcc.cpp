d9ba5d270ca1b47bc8f47a1a1e782f951cd4d6fb
Bruno Brito <bruno.debrito@tudelft.nl>
2018-11-08 13:54:51 +0100

    remove collision constraints

--- examples/ocp/lmpcc.cpp
+++ examples/ocp/lmpcc.cpp
@@ -87,29 +87,8 @@ int main( ){
 	// Wp: for the potential field associated with the obstacles in the cost
 	OnlineData wP;
 
-	OnlineData r_disc;
-	OnlineData disc_pos_0;
-    //Description of Obstacle 1
-	OnlineData obst1_x;
-	OnlineData obst1_y;
-	OnlineData obst1_theta;
-	// obst1_major:  major axis of the ellipse defining the obstacle
-	OnlineData obst1_major;
-	// obst1_minor:  minor axis of the ellipse defining the obstacle
-	OnlineData obst1_minor;
-
-    //Description of Obstacle 1
-	OnlineData obst2_x;
-	OnlineData obst2_y;
-	OnlineData obst2_theta;
-	OnlineData obst2_major;
-	OnlineData obst2_minor;
 	// Wv: weight on the desired acceleration
 	OnlineData Wv;
-	OnlineData disc_pos_1;
-	OnlineData disc_pos_2;
-	OnlineData disc_pos_3;
-	OnlineData disc_pos_4;
 
 	Expression lambda = 1/(1 + exp((s - d)/0.1));
 
@@ -166,90 +145,6 @@ int main( ){
 	Expression error_lag       = -dx_path_norm * (x - x_path) - dy_path_norm * (y - y_path);
 	
 	Expression road_boundary = -dy_path_norm * (x - x_path) + dx_path_norm * (y - y_path);
-	
-	Expression R_car(2,2);
-	R_car(0,0) = cos(psi);
-	R_car(0,1) = -sin(psi);
-	R_car(1,0) = sin(psi);
-	R_car(1,1) = cos(psi);
-	
-	Expression CoG(2,1);
-	CoG(0) = x;
-	CoG(1) = y; 
-	
-	Expression shift_1(2,1);
-	shift_1(0) = disc_pos_0;//-0.255;
-	shift_1(1) = 0;
-	Expression shift_2(2,1);
-	shift_2(0) = disc_pos_1;//2.64;
-	shift_2(1) = 0;
-	Expression shift_3(2,1);
-	shift_3(0) = disc_pos_2;//4.4;
-	shift_3(1) = 0;
-	Expression shift_4(2,1);
-	shift_4(0) = disc_pos_3;//4.4;
-	shift_4(1) = 0;
-	Expression shift_5(2,1);
-	shift_5(0) = disc_pos_4;//4.4;
-	shift_5(1) = 0;
-	
-    Expression position_disc_1(2,1);
-	position_disc_1 = CoG+R_car*shift_1;
-	
-	Expression position_disc_2(2,1);
-	position_disc_2 = CoG+R_car*shift_2;
-	
-	Expression position_disc_3(2,1);
-	position_disc_3 = CoG+R_car*shift_3;
-	
-	Expression position_disc_4(2,1);
-	position_disc_4 = CoG+R_car*shift_4;
-	
-	Expression position_disc_5(2,1);
-	position_disc_5 = CoG+R_car*shift_5;
-	
-	// For Obstacle 1
-	
-	Expression CoG_obst1(2,1);
-	CoG_obst1(0) = obst1_x;
-	CoG_obst1(1) = obst1_y;
-	
-	Expression deltaPos_disc_1_obstacle_1(2,1);
-	deltaPos_disc_1_obstacle_1 =  position_disc_1 - CoG_obst1;
-	
-	Expression deltaPos_disc_2_obstacle_1(2,1);
-	deltaPos_disc_2_obstacle_1 =  position_disc_2 - CoG_obst1;
-	
-	Expression deltaPos_disc_3_obstacle_1(2,1);
-	deltaPos_disc_3_obstacle_1 =  position_disc_3 - CoG_obst1;
-	
-	Expression deltaPos_disc_4_obstacle_1(2,1);
-	deltaPos_disc_4_obstacle_1 =  position_disc_4 - CoG_obst1;
-	
-	Expression deltaPos_disc_5_obstacle_1(2,1);
-	deltaPos_disc_5_obstacle_1 =  position_disc_5 - CoG_obst1;
-	
-	// For Obstacle 2
-	
-	Expression CoG_obst2(2,1);
-	CoG_obst2(0) = obst2_x;
-	CoG_obst2(1) = obst2_y;
-	
-	Expression deltaPos_disc_1_obstacle_2(2,1);
-	deltaPos_disc_1_obstacle_2 =  position_disc_1 - CoG_obst2;
-	
-	Expression deltaPos_disc_2_obstacle_2(2,1);
-	deltaPos_disc_2_obstacle_2 =  position_disc_2 - CoG_obst2;
-	
-	Expression deltaPos_disc_3_obstacle_2(2,1);
-	deltaPos_disc_3_obstacle_2 =  position_disc_3 - CoG_obst2;
-	
-    Expression deltaPos_disc_4_obstacle_2(2,1);
-	deltaPos_disc_4_obstacle_2 =  position_disc_4 - CoG_obst2;
-	
-	Expression deltaPos_disc_5_obstacle_2(2,1);
-	deltaPos_disc_5_obstacle_2 =  position_disc_5 - CoG_obst2;
-	
 
 
     ocp.subjectTo( -6 <= a <= 1.5 );
@@ -260,67 +155,11 @@ int main( ){
 	ocp.subjectTo(road_boundary -sv<= 6);
 	ocp.subjectTo(-road_boundary-sv<= 6);
 
-    // DEFINE COLLISION CONSTRAINTS:
-	// ---------------------------------------
 
-	Expression ab_1(2,2);
-	ab_1(0,0) = 1/((obst1_major + r_disc)*(obst1_major + r_disc));
-	ab_1(0,1) = 0;
-	ab_1(1,1) = 1/((obst1_minor + r_disc)*(obst1_minor + r_disc));
-	ab_1(1,0) = 0;
-
-	Expression ab_2(2,2);
-	ab_2(0,0) = 1/((obst2_major + r_disc)*(obst2_major + r_disc));
-	ab_2(0,1) = 0;
-	ab_2(1,1) = 1/((obst2_minor + r_disc)*(obst2_minor + r_disc));
-	ab_2(1,0) = 0;
-
-	Expression R_obst_1(2,2);
-	R_obst_1(0,0) = cos(obst1_theta);
-	R_obst_1(0,1) = -sin(obst1_theta);
-	R_obst_1(1,0) = sin(obst1_theta);
-	R_obst_1(1,1) = cos(obst1_theta);
-
-	Expression R_obst_2(2,2);
-	R_obst_2(0,0) = cos(obst2_theta);
-	R_obst_2(0,1) = -sin(obst2_theta);
-	R_obst_2(1,0) = sin(obst2_theta);
-	R_obst_2(1,1) = cos(obst2_theta);
-	
-
-	Expression c_disc_1_obst_1, c_disc_2_obst_1, c_disc_3_obst_1,  c_disc_4_obst_1,  c_disc_5_obst_1, c_disc_1_obst_2, c_disc_2_obst_2, c_disc_3_obst_2, c_disc_4_obst_2, c_disc_5_obst_2;
-	c_disc_1_obst_1 = deltaPos_disc_1_obstacle_1.transpose() * R_obst_1.transpose() * ab_1 * R_obst_1 * deltaPos_disc_1_obstacle_1;
-	c_disc_2_obst_1 = deltaPos_disc_2_obstacle_1.transpose() * R_obst_1.transpose() * ab_1 * R_obst_1 * deltaPos_disc_2_obstacle_1;
-	c_disc_3_obst_1 = deltaPos_disc_3_obstacle_1.transpose() * R_obst_1.transpose() * ab_1 * R_obst_1 * deltaPos_disc_3_obstacle_1;	
-	c_disc_4_obst_1 = deltaPos_disc_4_obstacle_1.transpose() * R_obst_1.transpose() * ab_1 * R_obst_1 * deltaPos_disc_4_obstacle_1;
-	c_disc_5_obst_1 = deltaPos_disc_5_obstacle_1.transpose() * R_obst_1.transpose() * ab_1 * R_obst_1 * deltaPos_disc_5_obstacle_1;
-	
-	c_disc_1_obst_2 = deltaPos_disc_1_obstacle_2.transpose() * R_obst_2.transpose() * ab_2 * R_obst_2 * deltaPos_disc_1_obstacle_2;
-	c_disc_2_obst_2 = deltaPos_disc_2_obstacle_2.transpose() * R_obst_2.transpose() * ab_2 * R_obst_2 * deltaPos_disc_2_obstacle_2;
-	c_disc_3_obst_2 = deltaPos_disc_3_obstacle_2.transpose() * R_obst_2.transpose() * ab_2 * R_obst_2 * deltaPos_disc_3_obstacle_2;
-	c_disc_4_obst_2 = deltaPos_disc_4_obstacle_2.transpose() * R_obst_2.transpose() * ab_2 * R_obst_2 * deltaPos_disc_4_obstacle_2;
-	c_disc_5_obst_2 = deltaPos_disc_5_obstacle_2.transpose() * R_obst_2.transpose() * ab_2 * R_obst_2 * deltaPos_disc_5_obstacle_2;
-
-	//ocp.subjectTo(c_disc_1_obst_1 + sv >= 1);
-	//ocp.subjectTo(c_disc_2_obst_1 + sv >= 1);
-	//ocp.subjectTo(c_disc_3_obst_1 + sv >= 1);
-	//ocp.subjectTo(c_disc_1_obst_2 + sv >= 1);
-	//ocp.subjectTo(c_disc_2_obst_2 + sv >= 1);
-	//ocp.subjectTo(c_disc_3_obst_2 + sv >= 1);
-	ocp.subjectTo(c_disc_1_obst_1 + sv >= 1);
-	ocp.subjectTo(c_disc_2_obst_1 + sv >= 1);
-	ocp.subjectTo(c_disc_3_obst_1 + sv >= 1);
-	ocp.subjectTo(c_disc_4_obst_1 + sv >= 1);
-	ocp.subjectTo(c_disc_5_obst_1 + sv >= 1);
-	ocp.subjectTo(c_disc_1_obst_2 + sv >= 1);
-	ocp.subjectTo(c_disc_2_obst_2 + sv >= 1);
-	ocp.subjectTo(c_disc_3_obst_2 + sv >= 1);
-	ocp.subjectTo(c_disc_4_obst_2 + sv >= 1);
-	ocp.subjectTo(c_disc_5_obst_2 + sv >= 1);
 	ocp.subjectTo(sv >= 0);
 	
 	//	ocp.minimizeLagrangeTerm(Wx*error_contour*error_contour + Wy*error_lag*error_lag +Wv*(v-vref)*(v-vref) +Wa*a*a+ Wdelta*(delta)*(delta)+ws*sv+wP*((1/(deltaPos_disc_1_obstacle_1.transpose()*deltaPos_disc_1_obstacle_1+0.01))+((1/(deltaPos_disc_2_obstacle_1.transpose()*deltaPos_disc_2_obstacle_1+0.01)))+((1/(deltaPos_disc_3_obstacle_1.transpose()*deltaPos_disc_3_obstacle_1+0.01)))+(1/(deltaPos_disc_1_obstacle_2.transpose()*deltaPos_disc_1_obstacle_2+0.01))+((1/(deltaPos_disc_2_obstacle_2.transpose()*deltaPos_disc_2_obstacle_2+0.01)))+((1/(deltaPos_disc_3_obstacle_2.transpose()*deltaPos_disc_3_obstacle_2+0.01))))); // weight this with the physical cost!!!
-	ocp.minimizeLagrangeTerm(Wx*error_contour*error_contour + Wy*error_lag*error_lag +Wv*(v-vref)*(v-vref) +Wa*a*a+ Wdelta*(delta)*(delta)+ws*sv+wP*(1/((1-c_disc_1_obst_1)*(1-c_disc_1_obst_1)+.001))+wP*(1/((1-c_disc_2_obst_1)*(1-c_disc_2_obst_1)+.001))+wP*(1/((1-c_disc_3_obst_1)*(1-c_disc_3_obst_1)+.001))+wP*(1/((1-c_disc_4_obst_1)*(1-c_disc_4_obst_1)+.001))+wP*(1/((1-c_disc_5_obst_1)*(1-c_disc_5_obst_1)+.001))+wP*(1/((1-c_disc_1_obst_2)*(1-c_disc_1_obst_2)+.001))+wP*(1/((1-c_disc_2_obst_2)*(1-c_disc_2_obst_2)+.001))+wP*(1/((1-c_disc_3_obst_2)*(1-c_disc_3_obst_2)+.001))+wP*(1/((1-c_disc_3_obst_2)*(1-c_disc_3_obst_2)+.001))+wP*(1/((1-c_disc_4_obst_2)*(1-c_disc_4_obst_2)+.001))+wP*(1/((1-c_disc_5_obst_2)*(1-c_disc_5_obst_2)+.001))); // weight this with the physical cost!!!
+	ocp.minimizeLagrangeTerm(Wx*error_contour*error_contour + Wy*error_lag*error_lag +Wv*(v-vref)*(v-vref) +Wa*a*a+ Wdelta*(delta)*(delta)+ws*sv); // weight this with the physical cost!!!
 	ocp.setModel(f);
 
 
