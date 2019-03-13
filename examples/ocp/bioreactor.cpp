6e45cb29c69f464e038a1f7468a0ec0af1ff4e14
Bruno Brito <bruno.debrito@tudelft.nl>
2019-03-13 14:50:40 +0100

    changes with babuska

--- examples/ocp/bioreactor.cpp
+++ examples/ocp/bioreactor.cpp
@@ -40,13 +40,13 @@ int main( ){
 
     // INTRODUCE THE VARIABLES:
     // -------------------------
-    DifferentialState     x,y,theta,dummy1,dummy2;
-    Control               v,w,sv1,sv2;
+    DifferentialState     x,y,theta;//,v;
+    Control               v,w;
     DifferentialEquation  f;
 
-	OnlineData x_goal;
-	OnlineData y_goal;
-	OnlineData theta_goal;
+    OnlineData x_goal;
+    OnlineData y_goal;
+    OnlineData theta_goal;
 
 	OnlineData Wx;
 	OnlineData Wy;
@@ -56,110 +56,39 @@ int main( ){
 
 	OnlineData vref;
 
-	OnlineData ws;
-	OnlineData wP;
-
-	OnlineData r_disc;
-	OnlineData disc_pos;
-
-	OnlineData obst1_x;
-	OnlineData obst1_y;
-	OnlineData obst1_theta;
-	OnlineData obst1_major;
-	OnlineData obst1_minor;
-
-	OnlineData obst2_x;
-	OnlineData obst2_y;
-	OnlineData obst2_theta;
-	OnlineData obst2_major;
-	OnlineData obst2_minor;
-
-	OnlineData collision_free_a1x;
-	OnlineData collision_free_a2x;
-	OnlineData collision_free_a3x;
-	OnlineData collision_free_a4x;
-
-	OnlineData collision_free_a1y;
-	OnlineData collision_free_a2y;
-	OnlineData collision_free_a3y;
-	OnlineData collision_free_a4y;
-
-	OnlineData collision_free_C1;
-	OnlineData collision_free_C2;
-	OnlineData collision_free_C3;
-	OnlineData collision_free_C4;
-
     // DEFINE A DIFFERENTIAL EQUATION:
     // -------------------------------
     
     f << dot(x) == v*cos(theta);
     f << dot(y) == v*sin(theta);
     f << dot(theta) == w;
-	f << dot(dummy1) == sv1;
-    f << dot(dummy2) == sv2;
+    //f << dot(v) == a;
+	//f << dot(dummy1) == sv1;
+    //f << dot(dummy2) == sv2;
 
     // DEFINE AN OPTIMAL CONTROL PROBLEM:
     // ----------------------------------
-    OCP ocp( 0.0, 5, 25.0 );
+    OCP ocp( 0.0, 20, 100.0 );
 
     // Need to set the number of online variables!
     ocp.setNOD(35);
 
-	ocp.minimizeLagrangeTerm(Wx*(x-x_goal)*(x-x_goal) + ws*sv1*sv1 + ws*sv2*sv2 + Wy*(y-y_goal)*(y-y_goal) + Wtheta*(theta-theta_goal)*(theta-theta_goal)+ Ww*w*w +Wv*(v-vref)*(v-vref) + wP*((1/((x-obst1_x)*(x-obst1_x)+(y-obst1_y)*(y-obst1_y)+0.0001)) + (1/((x-obst2_x)*(x-obst2_x)+(y-obst2_y)*(y-obst2_y)+0.0001)))); // weight this with the physical cost!!!
-	ocp.setModel(f);
+	ocp.minimizeLagrangeTerm(Wx*(x-x_goal)*(x-x_goal) +Wy*(y-y_goal)*(y-y_goal)+ Wtheta*(theta-theta_goal)*(theta-theta_goal)); //
+    ocp.minimizeMayerTerm(Wx*(x-x_goal)*(x-x_goal) +Wy*(y-y_goal)*(y-y_goal)+ Wtheta*(theta-theta_goal)*(theta-theta_goal)); //
 
-    ocp.subjectTo( -2.0 <= v <= 2.0 );
-    ocp.subjectTo( -1.0 <= w <= 1.0 );
+    ocp.setModel(f);
 
-    // DEFINE COLLISION CONSTRAINTS:
-	// ---------------------------------------
+    ocp.subjectTo( 0 <= v <= 0.2 );
+    ocp.subjectTo( -0.5 <= w <= 0.5 );
 
-	Expression ab_1(2,2);
-	ab_1(0,0) = 1/((obst1_major + r_disc)*(obst1_major + r_disc));
-	ab_1(0,1) = 0;
-	ab_1(1,1) = 1/((obst1_minor + r_disc)*(obst1_minor + r_disc));
-	ab_1(1,0) = 0;
+    //ocp.subjectTo( AT_END, x-x_goal  == 0.0 );
+    //ocp.subjectTo( AT_END, y-y_goal == 0.0 );
+    //ocp.subjectTo( AT_END, theta-theta_goal == 0.0 );
 
-	Expression ab_2(2,2);
-	ab_2(0,0) = 1/((obst2_major + r_disc)*(obst2_major + r_disc));
-	ab_2(0,1) = 0;
-	ab_2(1,1) = 1/((obst2_minor + r_disc)*(obst2_minor + r_disc));
-	ab_2(1,0) = 0;
-
-	Expression R_obst_1(2,2);
-	R_obst_1(0,0) = cos(obst1_theta);
-	R_obst_1(0,1) = -sin(obst1_theta);
-	R_obst_1(1,0) = sin(obst1_theta);
-	R_obst_1(1,1) = cos(obst1_theta);
-
-	Expression R_obst_2(2,2);
-	R_obst_2(0,0) = cos(obst2_theta);
-	R_obst_2(0,1) = -sin(obst2_theta);
-	R_obst_2(1,0) = sin(obst2_theta);
-	R_obst_2(1,1) = cos(obst2_theta);
-
-	Expression deltaPos_disc_1(2,1);
-	deltaPos_disc_1(0) =  x - obst1_x;
-	deltaPos_disc_1(1) =  y - obst1_y;
-
-	Expression deltaPos_disc_2(2,1);
-	deltaPos_disc_2(0) =  x - obst2_x;
-	deltaPos_disc_2(1) =  y - obst2_y;
-
-	Expression c_obst_1, c_obst_2;
-	c_obst_1 = deltaPos_disc_1.transpose() * R_obst_1.transpose() * ab_1 * R_obst_1 * deltaPos_disc_1;
-	c_obst_2 = deltaPos_disc_2.transpose() * R_obst_2.transpose() * ab_2 * R_obst_2 * deltaPos_disc_2;
-
-	ocp.subjectTo(c_obst_1 + sv1 >= 1);
-	ocp.subjectTo(c_obst_2 + sv1 >= 1);
-
-    ocp.subjectTo( x*collision_free_a1x + y*collision_free_a1y - collision_free_C1 + sv2 >= 0 );
-    ocp.subjectTo( x*collision_free_a2x + y*collision_free_a2y - collision_free_C2 + sv2 >= 0 );
-    ocp.subjectTo( x*collision_free_a3x + y*collision_free_a3y - collision_free_C3 + sv2 >= 0 );
-    ocp.subjectTo( x*collision_free_a4x + y*collision_free_a4y - collision_free_C4 + sv2 >= 0 );
 
     // DEFINE AN MPC EXPORT MODULE AND GENERATE THE CODE:
 	// ----------------------------------------------------------
+/*
 	OCPexport mpc( ocp );
 
 	mpc.set( HESSIAN_APPROXIMATION,            EXACT_HESSIAN  		);
@@ -178,6 +107,51 @@ int main( ){
 
 	mpc.exportCode( "ocp" );
 	mpc.printDimensionsQP( );
+*/
+    ocp.subjectTo( AT_START, x  == 0.9 );
+    ocp.subjectTo( AT_START, y  == 0.3 );
+    ocp.subjectTo( AT_START, theta  == -1.57 );
+    //ocp.subjectTo( AT_START, v  == 0.0 );
+    //ocp.subjectTo( AT_START, w == 0.0 );
+    double wx = 100;
+    double wy = 100;
+    double wtheta = 100;
+    double xgoal = 0.5;
+    double ygoal = 0.5;
+    double thetagoal = 0;
+    ocp.minimizeLagrangeTerm(v*v+w*w);//wx*(x-xgoal)*(x-xgoal) +wy*(y-ygoal)*(y-ygoal)+ wtheta*(theta-thetagoal)*(theta-thetagoal)); //
+    ocp.minimizeMayerTerm(wx*(x-xgoal)*(x-xgoal) +wy*(y-ygoal)*(y-ygoal)+ wtheta*(theta-thetagoal)*(theta-thetagoal)); //
+    // Additionally, flush a plotting object
+
+    GnuplotWindow window1;//( PLOT_AT_EACH_ITERATION );
+    window1.addSubplot( x,y, " X [m]" );
+    window1.addSubplot( y," Y [m]" );
+    window1.addSubplot( theta," Theta [m]" );
+    window1.addSubplot( v, "Velocity [m/s]" );
+    //window1.addSubplot( a, "Velocity [m/s^2]" );
+    window1.addSubplot( w,"Angular Velocity [m/s]" );
+    // DEFINE AN OPTIMIZATION ALGORITHM AND SOLVE THE OCP:
+    // --------------------------------------------------
+    OptimizationAlgorithm algorithm(ocp);
+
+    algorithm << window1;
+
+    // algorithm.set( HESSIAN_APPROXIMATION, EXACT_HESSIAN );
+    //  algorithm.set( HESSIAN_APPROXIMATION, GAUSS_NEWTON );
+    //algorithm.set( HESSIAN_APPROXIMATION, EXACT_HESSIAN );
+    algorithm.set( DISCRETIZATION_TYPE,              MULTIPLE_SHOOTING 	);
+   // algorithm.set( INTEGRATOR_TYPE,                  INT_RK4			    );
+    //algorithm.set( NUM_INTEGRATOR_STEPS,             25            		);
+    algorithm.set( QP_SOLVER,                        QP_QPOASES3    		);
+    //algorithm.set( HOTSTART_QP,                      NO             		);
+    //algorithm.set( SPARSE_QP_SOLUTION, 		       FULL_CONDENSING_N2	);
+    //algorithm.set( INTEGRATOR_TOLERANCE, 1e-8 );
+    algorithm.set( KKT_TOLERANCE, 1e-9 );
+    //algorithm.set( GLOBALIZATION_STRATEGY, GS_FULLSTEP );
+    //algorithm.set( MAX_NUM_ITERATIONS, 1 );
+
+    algorithm.solve();
+
 	// ----------------------------------------------------------
 
 
